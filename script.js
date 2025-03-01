0dcdd84f63704cb93af46e2bb8da9b37e0ec185d
aadityabhusal <bhusal.001aditya@gmail.com>
2025-03-01 17:32:58 +0545

    update: content cleanup

--- script.js
+++ script.js
@@ -1,55 +1,9 @@
-var menu = document.getElementById("menu");
-var scrollToTop = document.getElementById("scroll-to-top");
-
-document
-  .getElementsByClassName("menu-open")[0]
-  .addEventListener("click", function () {
-    menu.style.transform = "translate(0,0)";
-  });
-
-document
-  .getElementsByClassName("menu-close")[0]
-  .addEventListener("click", function () {
-    menu.style.transform = "translate(100vw,0)";
-  });
-
-document.addEventListener("scroll", function () {
-  var winHeight = (60 / 100) * window.innerHeight;
-  var imgLogo = document.getElementsByClassName("home-image-logo")[0];
-
-  if (window.scrollY > winHeight) {
-    imgLogo.style.transform = "translate(0,0)";
-    scrollToTop.style.display = "block";
-  } else {
-    imgLogo.style.transform = "translate(-150px, 0)";
-    scrollToTop.style.display = "none";
-  }
-});
-
-var header = document.getElementById("main-bg");
-function scroll() {
-  var offset = window.scrollY;
-  var rate = offset / 2;
-  header.style.backgroundPosition = "50% calc(50% + " + rate + "px)";
-}
-
-document.addEventListener("scroll", scroll);
-
-scrollToTop.addEventListener("click", function () {
-  window.scrollTo({
-    top: 0,
-    left: 0,
-    behavior: "smooth",
-  });
-});
-
-var headLink = document.getElementsByClassName("head-link");
-for (var i = 0; i < headLink.length; i++) {
+const headLink = document.getElementsByClassName("head-link");
+for (let i = 0; i < headLink.length; i++) {
   headLink[i].addEventListener("click", function () {
-    menu.style.transform = "translate(100vw,0)";
-    var clickSection = this.getAttribute("data-section");
-    var scrollToElement = document.getElementById(clickSection);
-    var topOffset = scrollToElement.offsetTop;
+    const clickSection = this.getAttribute("data-section");
+    const scrollToElement = document.getElementById(clickSection);
+    const topOffset = scrollToElement.offsetTop;
     window.scrollTo({
       top: topOffset - 60,
       left: 0,
@@ -57,5 +11,12 @@ for (var i = 0; i < headLink.length; i++) {
     });
   });
 }
+
+document.addEventListener("scroll", function () {
+  const winHeight = (25 / 100) * window.innerHeight;
+  const imgLogo = document.getElementsByClassName("home-image-logo")[0];
+  imgLogo.style.transform =
+    window.scrollY > winHeight ? "translate(0,0)" : "translate(-150px, 0)";
+});
 
 document.getElementById("foot-year").innerHTML = new Date().getFullYear();
